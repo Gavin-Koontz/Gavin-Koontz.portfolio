86d2cb5390f6ac9a5657034a3aabdd70a70d6cef
Gavin-Koontz <2023gkoontz@gmail.com>
2023-11-14 21:59:52 -0600

    added a project to the Projects Section. also added css to the projects page.

--- DisplayHidden.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const container = document.querySelector(".container");
-    const content = document.querySelector(".content");
-    const toggleButton = document.getElementById("toggleButton");
-
-    toggleButton.addEventListener("click", function () {
-        content.classList.toggle("expanded");
-    });
-});
